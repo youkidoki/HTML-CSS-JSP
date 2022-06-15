14a37a940b630dd683a4440dab6500f39e1ff8e1
youkidoki <youkidoki@naver.com>
2022-06-15 18:34:08 +0900

    ver 0.8

---
+++ router/API2.js
@@ -0,0 +1,17 @@
+var xhr = new XMLHttpRequest();
+var url = 'http://apis.data.go.kr/1471000/FoodNtrIrdntInfoService1/getFoodNtrItdntList1'; /*URL*/
+var queryParams = '?' + encodeURIComponent('serviceKey') + '='+'ODU3IOJuwhlQpDyH%2BtzJTxvn3SgcoAT2ndARyEEdbdiDzeM5upHkjGO5H2J5ZIuNtREOc0%2FE4N6cGE77C5Wrrg%3D%3D'; /*Service Key*/
+queryParams += '&' + encodeURIComponent('desc_kor') + '=' + encodeURIComponent('바나나칩'); /**/
+queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('1'); /**/
+queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('3'); /**/
+queryParams += '&' + encodeURIComponent('bgn_year') + '=' + encodeURIComponent('2017'); /**/
+queryParams += '&' + encodeURIComponent('animal_plant') + '=' + encodeURIComponent('(유)돌코리아'); /**/
+queryParams += '&' + encodeURIComponent('type') + '=' + encodeURIComponent('xml'); /**/
+xhr.open('GET', url + queryParams);
+xhr.onreadystatechange = function () {
+    if (this.readyState == 4) {
+        alert('Status: '+this.status+'nHeaders: '+JSON.stringify(this.getAllResponseHeaders())+'nBody: '+this.responseText);
+    }
+};
+
+xhr.send('');
